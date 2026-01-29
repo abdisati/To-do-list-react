5eb758bf1a48c4ee344eafd3c2e9ec9dbfe11577
Abdi Deresa <abdishegute@gmail.com>
2026-01-28 18:47:35 -0800

    initialized a todoapp using react framework and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+
+const tasks=Json.parse(localStorage.getItem('tasks')) || [];
+
+function ToDoApp(){
+
+}
+
+
+
+
+
+
+
+
+
+
+function App() {
+return <h1 className="text-9xl">Hello World</h1>
+}
+
+
+
+export default App
