63fe4cdfde786ddd666ec8e62555023690154800
Abdi Deresa <abdishegute@gmail.com>
2026-01-31 11:42:46 -0800

    added function to delet and add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,88 @@
-
+import { forwardRef,useRef, useState } from "react";
 
 
 //manage the tasks i.e storing the tasks
-const tasks=JSON.parse(localStorage.getItem("tasks")) || [];
+// const task={
+//   text:'cook food',
+//   completed:false
+// }
 
-function ListItem({task,handleDelete}){
-  return(
-    <li><span className="text-red-500">{task.text}</span><button onClick={handleDelete}>Delete</button></li>
-  );
+
+// localStorage.setItem('tasks',JSON.stringify([task]));
+
+// const tasks=JSON.parse(localStorage.getItem('tasks')) || [];
+
+
+function ListItem({task,handleDelete,index}){
+  const listStyle={
+    textDecoration:`${task.completed?'line-through':'none'}`
+  }
+  return <li  key={task.text}><span style={listStyle}>{task.text}</span> <button className="btn text-xl" onClick={()=>handleDelete(index)}>Delete</button></li>;
 }
-function DisplayTodos({tasks}){
+
+function DisplayTodos({tasks, handleDelete}){
   const lists=tasks.map((task,index)=>{
 return (
-   <ListItem task={task} handleDelete={(index)=>handleDelete(index)}/>
+   <ListItem task={task} handleDelete={handleDelete} index={index}/>
 );
   });
+  console.log(lists);
 return(
-  <div>
-    {lists}
+  <div className="text-2xl px-4 mt-2">
+    <ol className="list-disc px-2">
+     {lists}
+    </ol>
+    
   </div>
 );
 }
 
 
 
-function InputDisplay(){
-  return(
-    <div className="flex flex-row justify-around border-2 px-2 py-1 gap-1 mt-2  w-auto color">
-      <input type="text" placeholder="type the task..." className="border-2 flex-1 text-2xl"/> <button className="btn">Add</button>
-    </div>
-  );
-}
+// function InputDisplay(){
+//   return(
+//     <div className="flex flex-row justify-around border-2 px-2 py-1 gap-1 mt-2  w-auto color">
+//       <input type="text" placeholder="type the task..." className="border-2 flex-1 text-2xl"/> <button className="btn">Add</button>
+//     </div>
+//   );
+// }
+
+const InputDisplay=forwardRef((props,ref)=>{
+  return (<div className="flex flex-row justify-around border-2 px-2 py-1 gap-1 mt-2  w-auto color">
+      <input type="text" placeholder="type the task..." className="border-2 flex-1 text-2xl" ref={ref}/> <button className="btn" onClick={props.handleClick}>Add</button>
+    </div>);
+
+  });
 
 function ToDoApp(){
+  const [tasks,setTasks]=useState([]);
+  const InputRef=useRef(null);
+function handleClick(){
+  //this adds a new task to the task array
+  const value=InputRef.current.value.trim();
+
+  //if empty return
+  if(!value) return;
+  //create a task object
+  const task={
+    text:value,
+    completed:false
+  }
+  //now save the task to the task list array
+  setTasks((prevTasks)=>[...prevTasks,task]);
+
+  
+}
+
+function handleDelete(index){
+  const prevTasks=tasks.slice();
+  prevTasks.splice(index,1);
+  setTasks(prevTasks);
+}
   return (
     <div className="w-100 min-h-24 border-2 px-1">
-        <InputDisplay/>
-        <DisplayTodos/>
+        <InputDisplay ref={InputRef} handleClick={handleClick} />
+        <DisplayTodos tasks={tasks} handleDelete={handleDelete}/>
     </div>
   
   
